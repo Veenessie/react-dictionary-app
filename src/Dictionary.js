db5a3191ce24cf68c16c2f88c8e014675f24c0f1
Verity <mastersverity@gmail.com>
2024-04-15 12:21:05 +0100

    Intergrated API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, SetKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`🔎 Searching for the definition of ${keyword}`);
+
+    let apiKey = "7b12a4c8c3fba7979089o3b3ff6teab1";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function changeKeyword(event) {
